fd0a691384f3e7f299c46c4a7b53e6f6a13f6126
Jon Wu <jon@paperculture.com>
2014-06-09 12:27:00 -0700

    Generate files into app/vendor and focus on LESS changes only

--- lib/generators/flatuipro/demo/demo_generator.rb
+++ lib/generators/flatuipro/demo/demo_generator.rb
@@ -5,11 +5,11 @@ module Flatuipro
   module Generators
     class DemoGenerator < Rails::Generators::Base
       desc "Setup Flat UI Pro Demo page."
-      source_root File.expand_path("../../../../../app/assets/demo", __FILE__)
+      source_root "vendor/assets/demo"
 
       # Detect if Flat UI Pro assets copied over to gem
       def check_flatuipro_install
-        unless File.exist?(File.expand_path("../../../../../app/assets", __FILE__))
+        unless File.exist?("vendor/assets/demo/index.html")
           raise "Please run install generator first"
         end
       end
@@ -23,7 +23,7 @@ module Flatuipro
         copy_file "index.html.erb", "app/views/flatuipro_demo/index.html.erb"
 
         # Add demo LESS
-        copy_file "flatuipro-demo.less", "app/assets/stylesheets/flatuipro-demo.less"
+        copy_file "flatuipro-demo.less", "vendor/assets/stylesheets/flatuipro-demo.less"
         
         # Handle CSS Manifest
         css_manifest = "app/assets/stylesheets/application.css"
@@ -39,7 +39,7 @@ module Flatuipro
         copy_file "flatuipro-demo.js", "app/assets/javascripts/flatuipro-demo.js"
 
         # Handle JS Manifest
-        js_manifest = "app/assets/javascripts/application.js"
+        js_manifest = "app/assets/javascripts/flatui-application.js"
         if File.exist?(js_manifest)
           content = File.read(js_manifest)
           unless content.match(/require_tree\s+\./)
--- lib/generators/flatuipro/install/install_generator.rb
+++ lib/generators/flatuipro/install/install_generator.rb
@@ -9,109 +9,105 @@ module Flatuipro
       source_root File.expand_path("../templates", __FILE__)
 
       def copy_assets
-        gem_assets_dir = File.expand_path("../../../../../app/assets", __FILE__)
+        vendor_assets_dir = "vendor/assets"
         pro_dir = flatuipro_dir
 
-        unless File.exist?(File.join(pro_dir, "index.html"))
-          pro_dir = File.join(pro_dir, "HTML/UI")
-        end
-        if File.directory?(File.join(pro_dir, "Flat-UI-Pro-1.2.5"))
-          pro_dir = File.join(pro_dir, "Flat-UI-Pro-1.2.5")
-        end
         unless File.directory?(pro_dir) && File.exist?(File.join(pro_dir, "index.html"))
           raise "Invalid Flat UI Pro directory"
         end
 
-        directory File.join(pro_dir, "less"),   File.join(gem_assets_dir, "less")
-        directory File.join(pro_dir, "js"),     File.join(gem_assets_dir, "javascripts")
-        directory File.join(pro_dir, "images"), File.join(gem_assets_dir, "images")
-        directory File.join(pro_dir, "fonts"),  File.join(gem_assets_dir, "fonts")
+        directory File.join(pro_dir, "less"),   File.join(vendor_assets_dir, "stylesheets")
+        directory File.join(pro_dir, "js"),     File.join(vendor_assets_dir, "javascripts"), :exclude_pattern => /application\.js/
+        directory File.join(pro_dir, "images"), File.join(vendor_assets_dir, "images")
+        directory File.join(pro_dir, "fonts"),  File.join(vendor_assets_dir, "fonts")
+
+        copy_file File.join(pro_dir, "js", "application.js"), File.join(vendor_assets_dir, "javascripts", "flatui-application.js")
 
         # Demo page assets
-        copy_file File.join(pro_dir, "index.html"),           File.join(gem_assets_dir, "demo", "index.html")
-        copy_file File.join(pro_dir, "js", "application.js"), File.join(gem_assets_dir, "demo", "flatuipro-demo.js")
-        copy_file File.join(pro_dir, "less", "demo.less"),    File.join(gem_assets_dir, "demo", "flatuipro-demo.less")
+        copy_file File.join(pro_dir, "index.html"),           File.join(vendor_assets_dir, "flat-ui-demo.html")
+        copy_file File.join(pro_dir, "less", "demo.less"),    File.join(vendor_assets_dir, "flat-ui-demo.less")
       end
 
       def add_assets
-        copy_file "flatuipro.js",   "app/assets/javascripts/flatuipro.js"
-        copy_file "flatuipro.less", "app/assets/stylesheets/flatuipro.less"
+        # PC: No need to create stub files. We have very customized imports for optimal file size.
+        # copy_file "flatuipro.js",   "app/assets/javascripts/flatuipro.js"
+        # copy_file "flatuipro.less", "app/assets/stylesheets/flatuipro.less"
 
-        # Handle JS Manifest
-        js_manifest = "app/assets/javascripts/application.js"
-        if File.exist?(js_manifest)
-          content = File.read(js_manifest)
-          unless content.match(/require\s+flatuipro/)
-            insert_into_file js_manifest, "//= require flatuipro\n", :after => "jquery\n"
-          end
-          unless content.match(/require\s+jquery.ui.all/)
-            unless content.match(/require\s+jquery.ui.effect/)
-              insert_into_file js_manifest, "//= require jquery.ui.effect\n", :after => "jquery\n"
-            end
-            unless content.match(/require\s+jquery.ui.tooltip/)
-              insert_into_file js_manifest, "//= require jquery.ui.tooltip\n", :after => "jquery\n"
-            end
-            unless content.match(/require\s+jquery.ui.spinner/)
-              insert_into_file js_manifest, "//= require jquery.ui.spinner\n", :after => "jquery\n"
-            end
-            unless content.match(/require\s+jquery.ui.slider/)
-              insert_into_file js_manifest, "//= require jquery.ui.slider\n", :after => "jquery\n"
-            end
-            unless content.match(/require\s+jquery.ui.datepicker/)
-              insert_into_file js_manifest, "//= require jquery.ui.datepicker\n", :after => "jquery\n"
-            end
-            unless content.match(/require\s+jquery.ui.button/)
-              insert_into_file js_manifest, "//= require jquery.ui.button\n", :after => "jquery\n"
-            end
-          end
-        else
-          copy_file "application.js", js_manifest
-        end
-
-        # Handle CSS Manifest
-        css_manifest = "app/assets/stylesheets/application.css"
-        if File.exist?(css_manifest)
-          content = File.read(css_manifest)
-          unless content.match(/require_tree\s+\./)
-            style_require_block = " *= require flatuipro\n"
-            insert_into_file css_manifest, style_require_block, :after => "require_self\n"
-          end
-        else
-          copy_file "application.css", "app/assets/stylesheets/application.css"
-        end
+      #  # Handle JS Manifest
+      #  js_manifest = "app/assets/javascripts/application.js"
+      #  if File.exist?(js_manifest)
+      #    content = File.read(js_manifest)
+      #    unless content.match(/require\s+flatuipro/)
+      #      insert_into_file js_manifest, "//= require flat-ui\n", :after => "jquery\n"
+      #    end
+      #    unless content.match(/require\s+jquery.ui.all/)
+      #      unless content.match(/require\s+jquery.ui.effect/)
+      #        insert_into_file js_manifest, "//= require jquery.ui.effect\n", :after => "jquery\n"
+      #      end
+      #      unless content.match(/require\s+jquery.ui.tooltip/)
+      #        insert_into_file js_manifest, "//= require jquery.ui.tooltip\n", :after => "jquery\n"
+      #      end
+      #      unless content.match(/require\s+jquery.ui.spinner/)
+      #        insert_into_file js_manifest, "//= require jquery.ui.spinner\n", :after => "jquery\n"
+      #      end
+      #      unless content.match(/require\s+jquery.ui.slider/)
+      #        insert_into_file js_manifest, "//= require jquery.ui.slider\n", :after => "jquery\n"
+      #      end
+      #      unless content.match(/require\s+jquery.ui.datepicker/)
+      #        insert_into_file js_manifest, "//= require jquery.ui.datepicker\n", :after => "jquery\n"
+      #      end
+      #      unless content.match(/require\s+jquery.ui.button/)
+      #        insert_into_file js_manifest, "//= require jquery.ui.button\n", :after => "jquery\n"
+      #      end
+      #    end
+      #  else
+      #    copy_file "application.js", js_manifest
+      #  end
+      #
+      #  # Handle CSS Manifest
+      #  css_manifest = "app/assets/stylesheets/application.css"
+      #  if File.exist?(css_manifest)
+      #    content = File.read(css_manifest)
+      #    unless content.match(/require_tree\s+\./)
+      #      style_require_block = " *= require flat-ui\n"
+      #      insert_into_file css_manifest, style_require_block, :after => "require_self\n"
+      #    end
+      #  else
+      #    copy_file "application.css", "app/assets/stylesheets/application.css"
+      #  end
       end
 
       def patch_assets
-        gem_assets_dir = File.expand_path("../../../../../app/assets", __FILE__)
+        vendor_assets_dir = "vendor/assets"
 
         # LESS patches
         # switch.less
         # More involved patch because less-rails won't translate when inside ~""
         # Create LESS variable and interpolate into .mask(~"")
-        switch_file = File.join(gem_assets_dir, "less/modules", "switch.less")
+        switch_file = File.join(vendor_assets_dir, "stylesheets/modules", "switch.less")
         mask_image_url = "@mask-image-url: image-url('switch/mask.png');\n"
         insert_into_file switch_file, mask_image_url, :before => ".has-switch {"
         gsub_file switch_file, /url\('\.\.\/images\/.+?\)/, "@{mask-image-url}"
 
         # variables.less
-        gsub_file File.join(gem_assets_dir, "less", "variables.less"), /"..\/fonts\/lato\/"/, '"lato/"'
-        gsub_file File.join(gem_assets_dir, "less", "variables.less"), /"..\/fonts\/"/, '""'
+        gsub_file File.join(vendor_assets_dir, "stylesheets", "variables.less"), /"..\/fonts\/lato\/"/, '"lato/"'
+        gsub_file File.join(vendor_assets_dir, "stylesheets", "variables.less"), /"..\/fonts\/"/, '""'
 
         # local-fonts.less
-        gsub_file(File.join(gem_assets_dir, "less/modules", "local-fonts.less"), /~"url.+?"/) { |match|
+        gsub_file(File.join(vendor_assets_dir, "stylesheets/modules", "local-fonts.less"), /~"url.+?"/) { |match|
           match[2..-2].sub(/url\('(.+?)'\)/, "font-url('\\1')")
         }
 
         # glyphicons.less
-        gsub_file(File.join(gem_assets_dir, "less/modules", "glyphicons.less"), /~"url.+?"/) { |match|
+        gsub_file(File.join(vendor_assets_dir, "stylesheets/modules", "glyphicons.less"), /~"url.+?"/) { |match|
           match[2..-2].sub(/url\('(.+?)'\)/, "font-url('\\1')")
         }
 
         # Fix syntax error when precompiling assets
-        gsub_file File.join(gem_assets_dir, "less/modules", "caret.less"), ".scale(1.001);", "//.scale(1.001);"
+        #gsub_file File.join(vendor_assets_dir, "stylesheets/modules", "caret.less"), ".scale(1.001);", "//.scale(1.001);"
         
         # Demo page patches
-        file = File.join(gem_assets_dir, "demo", "index.html")
+        file = File.join(vendor_assets_dir, "flat-ui-demo.html")
         # Fix image links
         gsub_file file, /<img src="images\/.+?>/ do |s|
           match = /images\/(.+?)"/.match(s)
@@ -119,7 +115,7 @@ module Flatuipro
         end
 
         # Remove everything before <body> tag and after 'Load JS', inclusive
-        new_file = File.open("#{file}.erb", "w")
+        new_file = File.open("app/views/theme/index.html.erb", "w")
         include_line = false
         IO.foreach(file) do |line|
           include_line = false if line =~ /Load JS/
@@ -129,6 +125,9 @@ module Flatuipro
           include_line = true if line =~ /<body>/
         end
         new_file.close
+
+        # Delete intermediate file
+        remove_file file
       end
     end
   end
